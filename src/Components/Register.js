d4d502311baace25c1b047b5e97c1b2bf2ca66f4
kennedy-dan <61075930+kennedy-dan@users.noreply.github.com>
2022-04-26 21:11:45 +0100

    new commit

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -127,10 +127,6 @@ const Register = () => {
       aria-modal="true"
     >
       <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 ">
-        <div
-          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-          aria-hidden="true"
-        ></div>
 
         <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-3xl sm:w-full">
           <div className=" px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
